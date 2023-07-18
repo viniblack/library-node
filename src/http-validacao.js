732426265c6aa0fb0e69a14b27a5b7814c16b796
Vini Black <viniibl4ck@gmail.com>
2023-07-18 14:56:53 -0300

    finalizando validação dos links

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -1,7 +1,37 @@
+import chalk from "chalk";
+
 function extraiLinks(arrLinks) {
   return arrLinks.map((objetoLink) => Object.values(objetoLink).join());
 }
 
-export default function listaValidada(listaLinks) {
-  return extraiLinks(listaLinks);
+async function checaStatus(listaURL) {
+  const arrStatus = await Promise.all(
+    listaURL.map(async (url) => {
+      try {
+        const response = await fetch(url, { method: "HEAD" });
+        return response.status;
+      } catch (erro) {
+        return manejaErros(erro);
+      }
+    })
+  );
+  return arrStatus;
+}
+
+function manejaErros(erro) {
+  if (erro.cause.code === "ENOTFOUND") {
+    return "link não encontrado";
+  } else {
+    return "ocorreu algum erro";
+  }
+}
+
+export default async function listaValidada(listaLinks) {
+  const links = extraiLinks(listaLinks);
+  const status = await checaStatus(links);
+
+  return listaLinks.map((objeto, index) => ({
+    ...objeto,
+    status: status[index],
+  }));
 }
