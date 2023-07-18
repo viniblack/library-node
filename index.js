1e277b8fe68f4ef5320357dd00ed22fac0a70744
Vini Black <viniibl4ck@gmail.com>
2023-07-18 13:00:58 -0300

    usando regex para pegar o titulo e os links

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 import fs from "fs";
 import chalk from "chalk";
 
+function extraiLinks(texto) {
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const capturas = [...texto.matchAll(regex)];
+  const resultados = capturas.map((captura) => ({ [captura[1]]: captura[2] }));
+  return resultados;
+}
+
 function trataErro(erro) {
   throw new Error(chalk.red(erro.code, "Não foi possivel encontrar o arquivo"));
 }
 
-function pegaArquivo(caminho) {
-  const encoding = "utf-8";
-  fs.promises
-    .readFile(caminho, encoding)
-    .then((texto) => console.log(chalk.green(texto)))
-    .catch(trataErro);
+async function pegaArquivo(caminho) {
+  try {
+    const encoding = "utf-8";
+    const texto = await fs.promises.readFile(caminho, encoding);
+    console.log(extraiLinks(texto));
+  } catch (erro) {
+    trataErro(erro);
+  }
 }
 
 pegaArquivo("./arquivos/text.md");
